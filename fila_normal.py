cb0a630bf84d133c8a70e17f8015049c571040e7
Evandro P. Santos <evandropsantos@gmail.com>
2023-09-15 19:41:56 -0300

    refactory nome variavel

--- fila_normal.py
+++ fila_normal.py
@@ -3,7 +3,7 @@ from fila_base import FilaBase
 
 class FilaNormal(FilaBase):
     def gerar_senha_atual(self) -> None:
-        self.senhaatual = f'NM{self.codigo}'
+        self.senha_atual = f'NM{self.codigo}'
 
     def chama_cliente(self, caixa: int) -> str:
         cliente_atual: str = self.fila.pop(0)
